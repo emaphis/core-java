82cc298bd81fedf669c5b7b23ff9836f57fd7d24
Ed Maphis <emaphis85@gmail.com>
2018-06-01 21:21:17 -0400

    Bug fix.

--- src/main/java/vol1/chap02/sec08/PascalsTriangle1.java
+++ src/main/java/vol1/chap02/sec08/PascalsTriangle1.java
@@ -8,14 +8,14 @@ package vol1.chap02.sec08;
  */
 public class PascalsTriangle1 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 7;
         int[][] triangle = new int[n][];
 
         for (int i = 0; i < n; i++) {
             triangle[i] = new int[i+1];
             triangle[i][0] = 1;
             triangle[i][i] = 1;
-            for (int j = 0; j < i; j++) {
+            for (int j = 1; j < i; j++) {
                 triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
             }
         }
