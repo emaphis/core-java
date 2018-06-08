1a9ec863bccc8c911b461185665c42098948c4da
Ed Maphis <emaphis85@gmail.com>
2018-06-07 23:58:56 -0400

    Packages examples

---
+++ src/main/java/PackageTest/PackageTest.java
@@ -0,0 +1,22 @@
+/*
+ * Exercising the Package system.
+ */
+package PackageTest;
+
+import org.core.java.*;
+
+import static java.lang.System.*;
+
+/**
+ *
+ * @author emaphis
+ */
+public class PackageTest {
+   public static void main(String[] args) {
+      // because of the import statement, we don't have to use
+      // org.core.java.Employee here.
+      Employee harry = new Employee("Harry Haker", 0)
+
+
+   }
+}
